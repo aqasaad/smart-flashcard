2b6267aaf251d69404e1effe6705cce846dfbd24
jhebert <jhebert@f5c7f6fd-da19-0410-ab50-7f24de69d321>
2006-08-02 04:45:28 +0000

    More revisions. Now can estimate future probabilities if answered correctly or not.

--- flashcard.py
+++ flashcard.py
@@ -5,28 +5,25 @@ import random
 import time
 import particleFilter
 import gaussProcess
+import fileHandlers
 PRINT_PROB = True
 
 
 
-#parse the input file data into words and definitions
-def parseData(lines):
-    toReturn = []
-    for line in lines:
-        split = line.find('-')
-        if(split==-1):
-            continue
-        word = line[:split].strip()
-        defn = line[split+1:].strip()
-        if((len(word)>1) & (len(defn)>1)):
-            toReturn.append((word,convString(word),convString(defn)))
-    return toReturn
 
-def askWord(Uword):
+def askWord(Uword, defn, prevProb):
     print Uword
     if(PRINT_PROB):
         print prevProb
     user = raw_input('definition: ')
+    if(user == 'quit'):
+         return -1
+    outCome = getStringMatch(user, defn)
+    if(outCome == 1):
+        print 'Correct!'
+    else:
+        print defn       
+    return outCome
 
 
 
@@ -36,7 +33,7 @@ def loop(items):
     maxRange, curRange = len(items), 5
     for word in items:
         words[word[0]] = None
-    history, count = loadHistory(words)
+    history, count = fileHandlers.loadHistory(words)
     particleFilters, gauss = initLearning(history)
     pos, neg = gauss.binDataPoints(10)
     while(user != 'quit'):
@@ -46,16 +43,14 @@ def loop(items):
         index = min(len(items), int(random.random()*curRange))
         item = items[index]
         word, Uword, defn = item
-        askWord(Uword)
         prevProb = particleFilters[word].getBestEstimate()
-
-        if(user == 'quit'):
+        posChange = particleFilters[word].getPotentialOutcome(1, gauss)
+        negChange = particleFilters[word].getPotentialOutcome(0, gauss)
+        print 'potential pos change: ' + str(posChange)
+        print 'potential neg change: ' + str(negChange)
+        outCome = askWord(Uword, defn, prevProb) 
+        if(outCome == -1):
             break
-        outCome = getStringMatch(user, defn)
-        if(outCome == 1):
-            print 'Correct!'
-        else:
-            print defn
         history[word].append((count,outCome))
         gauss.addDataPoint(word, prevProb, outCome)
         particleFilters[word].factorOutcome(outCome, gauss)
@@ -65,7 +60,11 @@ def loop(items):
         if(PRINT_PROB):
             print particleFilters[word].getBestEstimate()
         print '****'
-    saveHistory(history)
+        # This shouldn't fuxor my histroy, right?
+        #if(len(history) > 10):
+        #    fileHandlers.saveHistory(history)
+        #    history, count = fileHandlers.loadHistory(words)
+    fileHandlers.saveHistory(history)
 
 
 def initLearning(history):
@@ -89,42 +88,6 @@ def initLearning(history):
     return (toReturn, gauss)
 
 
-def loadHistory(words):
-    toReturn, maxCount = {}, 0
-    for word in words:
-        toReturn[word] = []
-    try:
-        data = open('.history').read().split('\n')
-    except:
-        data = []
-    for line in data:
-        if(len(line)<3):
-            continue
-        word, items = line.split(' : ')
-        if(len(items)<3):
-            continue
-        if(not(word in words)):
-            continue
-        for item in items.split('('):
-            try:
-                first = int(item[:item.find(',')])
-                second = int(item[item.find(',')+2 : item.find(')')])
-                maxCount = max(first, maxCount)
-                toReturn[word].append((first, second))
-            except:
-                print 'item: '+str(item)
-    return (toReturn, maxCount)
-
-
-
-def saveHistory(history):
-    f = open('.history', 'w')
-    for word in history:
-        toWrite = ' '.join([str(a) for a in history[word]])
-        #print 'toAdd: '+str(toWrite)
-        f.write(str(word) + ' : ' + toWrite + '\n')        
-    f.close()
-
 
 
 #return a numerical score for how well the user's input matches the definition
@@ -137,10 +100,7 @@ def getStringMatch(w1, w2):
             if(getWordMatch(a,b)):
                 return 1
     return 0
-    #if(w1==w2):
-    #    return 1
-    #else:
-    #    return 0
+
 
 
 def getWordMatch(a,b):
@@ -151,49 +111,7 @@ def getWordMatch(a,b):
     return (na == nb)
 
 
-#convert the input data to unicode
-#e/, e\, e^, a^ are currently matched
-def convString(string):
-    toReturn = []
-    for s in string:
-        if(s=='/'):
-            if(toReturn[-1]=='e'):
-                toReturn[-1]=unichr(233)
-        elif(s=='\\'):
-            if(toReturn[-1]=='e'):
-                toReturn[-1]=unichr(232)
-        elif(s=='^'):
-            if(toReturn[-1]=='e'):
-                toReturn[-1]=unichr(234)
-            elif(toReturn[-1]=='a'):
-                toReturn[-1]=unichr(226)
-        else:
-            toReturn.append(s) 
-    return unicode(''.join(toReturn))
 
-
-#open .flashcard and see if a similar file name has been entered before
-def resolveFileName(name):
-    possible = []
-    try:
-        data = [a[5:] for a in open('.flashcard').read().split('\n') if(a.find('file:')>-1)]
-    except:
-        data = []
-    for item in data:
-        if(item.find(name)>-1):
-            possible.append(item)
-    if(len(possible)>0):
-       toReturn = possible[0]
-    else:
-        toReturn = name
-    try:
-        open(name).read()
-        file = open('.flashcard','a')
-        file.write('\nfile:'+name)
-        file.close()
-    except:
-        print ''
-    return toReturn
 
 
 #main program loop
@@ -206,9 +124,9 @@ def runQuestions():
     if(len(sys.argv)<2):
         print 'please specify an input file!'
     fileName = sys.argv[1]
-    name = resolveFileName(fileName)
+    name = fileHandlers.resolveFileName(fileName)
     data = open(name).read().split('\n')
-    items = parseData(data)
+    items = fileHandlers.parseData(data)
     loop(items)
 
     
