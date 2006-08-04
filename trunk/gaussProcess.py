58943c42e96ff7eaa65f1a2dfcbbac181f1b2eb6
jhebert <jhebert@f5c7f6fd-da19-0410-ab50-7f24de69d321>
2006-08-04 23:05:13 +0000

    Comments are wonderful things when used to describe the meaning of each function. Still should go through for code clarity.

--- trunk/gaussProcess.py
+++ trunk/gaussProcess.py
@@ -9,20 +9,36 @@ class gaussProcess:
 
 
     def __init__(self):
+        # These two lists hold the collected data points.
+        # The first holds incorrect (user) results and the
+        # second holds correct (user) results.
+        # The data is in the form of tuples:
+        # (estimated probability of being correctly answered,
+        # (whether the user did answer correctly,
+        # how many other words had been asked since previously
+        # asking this word).
         self.dataPoints = [[], []]
+        # This is a hash of words to tuples of probability, answer
+        # result, and the iteration count of the flashCard loop.
+        # This structure exists so that 'dataPoints' structure can
+        # be correctly maintained.
         self.prevData = {}
 
 
+    # Add a data point to the collection. This allows a relation to be
+    # built between esitamated probability, previously learning, and current
+    # answering of definitions.
     def addDataPoint(self, word, p, r, count):
         p = min(.999999, max(.000001, p))
         if(word in self.prevData):
             oldP, oldR, oldCount = self.prevData[word]
             self.dataPoints[oldR].append((oldP, r, count-oldCount))
-            #self.dataPoints[2].append((oldP, r, count-oldCount))
         self.prevData[word] = (p, r, count)
-        #self.dataPoints.append((p,r))
 
 
+    # Get the predicted probability weigted by normal distributions
+    # based off of the learned data points. Factors on outCome, probability
+    # estimate.
     def getResultantProb(self, pProb, outCome, word, count):
         sumWeights, sumResults = 0.0, 0.0
         data = self.dataPoints[outCome]
@@ -39,6 +55,9 @@ class gaussProcess:
             return (sumResults / sumWeights)
 
 
+    # Like above, but also incorporates number of iterations
+    # since tihs word was last asked - hence to incorporate
+    # gains via short term memory and such.
     def getBlankProb(self, pProb, outCome, word, count):
         sumResults2, sumWeights2 = 0.0, 0.0
         for item in self.dataPoints[outCome]:
--- trunk/particleFilter.py
+++ trunk/particleFilter.py
@@ -8,39 +8,47 @@ class particleFilter:
 
 
     def __init__(self, word, n):
+        #Variance when adding nose to selected particles in resampling.
         self.PARTICLE_VAR = .01
+        #The word for this particle filter.
         self.word = word
         #Number of particles in this particleFilter
         self.numParticles = n
         #Array to store particle locations
         self.particleArray = [0] * self.numParticles
+        #Init the particles into a uniform distribution.
         for i in range(len(self.particleArray)):
             self.particleArray[i] = random.random()
 
+
+    #Return a estimate probability for the distribution after the 'outCome' event.
     def getPotentialOutcome(self, outCome, gauss, count):
         particles = self.__reSample__(self.__getLikelihoods__(outCome), gauss, outCome, count)
         return self.__bestEstimateHelper__(particles)
 
+
+    #Factor the 'outCome' event into the probability distribution and save the results.
     def factorOutcome(self, outCome, gauss, count):
         particles = self.__reSample__(self.__getLikelihoods__(outCome), gauss, outCome, count)
         self.__updateParticleArray__(particles)
 
+
+    #Return the likelihoods for each particle for the event 'outCome'
     def __getLikelihoods__(self, outCome):
-        toReturn = []
         # if outCome = 0, then want 1 - p
         # if outCome = 1, then want p
-        #
         return [(1-outCome)+(2*outCome-1)*p for p in self.particleArray]
-        #for p in self.particleArray:
-        #    if(outCome == 0):
-        #        toReturn.append(1-p)
-        #    else:
-        #        toReturn.append(p)
-        #return toReturn
 
+
+    # Save 'particles' as the particleArray for this particleFilter
     def __updateParticleArray__(self, particles):
         self.particleArray = particles
 
+
+    # Resample the particles according to their likelihoods.
+    # This is done using a 'clock hand' type method. It is linear
+    # in complexity and in the limit is just as good. See Dieter
+    # Fox's CSE 473 slides Spring 2005, Robotics lecture.
     def __reSample__(self, likelihoods, gauss, outCome, count):
         toReturn = []
         s = sum(likelihoods)
@@ -67,11 +75,17 @@ class particleFilter:
             j += 1
         return toReturn
 
+
+    # Outside method indirectly calling internal method
+    # to determine the best estimate for the peak of this
+    # distribution.
     def getBestEstimate(self):
         return self.__bestEstimateHelper__(self.particleArray)
 
 
-
+    # Determine an estimate for the probability of this distribution
+    # by binning the particles into n discrete bins and then averaging
+    # the particles in the mode bin.
     def __bestEstimateHelper__(self, particles):
         hist = [0]*20
         sums = [0.0]*20
@@ -83,18 +97,7 @@ class particleFilter:
         for i in range(20):
             if(hist[i]==maxCount):
                 return sums[i] / hist[i]
-        #for i in range(20):
-        #    if(hist[i]==max(hist)):
-        #        maxI = i
-        #aSum, aCount = 0,0
-        #for p in particles:
-        #    if(min(19,int(p*20)) == maxI):
-        #        aSum += p
-        #        aCount += 1
-        #if(aCount==0):
-        #    print maxI
-        #    print hist
-        #return aSum / aCount
+
 
 
 
